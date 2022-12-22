f771c95ae6dd66fde50fa8a93183f5c967c9c99d
Pawankumar Sonwane <sonwanepawankumar@gmail.com>
2022-12-22 18:07:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,11 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #Noralize the json format into table like structure
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #Put the normslized json content into dataframe to represent as a table like structure
